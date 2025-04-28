4c1f19e7ed63801c2263f564dd5010cb42c9dbb7
ajmal123-coder <ajmalsadhath@gmail.com>
2025-04-28 11:37:58 +0530

    take biggest string and append the rest of the biggest

--- WEEK 2/Main.java
+++ WEEK 2/Main.java
@@ -3,25 +3,31 @@
 //Example :-
 //Input: word1 = "abc", word2 = "pqr"
 //Output: "apbqcr"
+//. Merge Strings Alternately
+//
+//Example :-
+//Input: word1 = "abc", word2 = "pqr"
+//Output: "apbqcr"
 public class Main {
     public static void main(String[] args) {
         Concat c = new Concat();
-        System.out.println(c.abc("abc","pqr"));
+        System.out.println(c.abc("abc","pqryht"));
     }
-
 }
 class Concat{
     public StringBuilder abc(String str1, String str2){
         StringBuilder str = new StringBuilder();
-        int len = 0;
-        if (str1.length() >= str2.length()){
-            len = str1.length();
-        }else {
-            len = str2.length();
-        }
-        for(int i = 0;i<=len-1;i++){
-            str.append(String.valueOf(str1.charAt(i)));
-            str.append(String.valueOf(str2.charAt(i)));
-        }
+        int i = 0;
+        int j = 0;
+       while (i< str1.length() || j < str2.length()){
+           if (i < str1.length()){
+               str.append(String.valueOf(str1.charAt(i)));
+               i++;
+           }
+           if (j < str2.length()){
+               str.append(String.valueOf(str2.charAt(j)));
+               j++;
+           }
+       }
         return str;
 }}
